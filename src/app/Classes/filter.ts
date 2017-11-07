528f78978364c1434371ed39947402d9cd0887cb
Mads <hypermads@gmail.com>
2017-11-07 12:21:08 +0100

    :bomb: filter works :bomb:

--- src/app/Classes/filter.ts
+++ src/app/Classes/filter.ts
@@ -4,6 +4,10 @@ import {Category} from './category';
 
 export class Filter {
 
+  static monthNames: string[] = ['January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+  
   dividedBy: string;
   sortBy: string;
 
@@ -13,15 +17,76 @@ export class Filter {
   showOnlyVisibleInNetBank: boolean;
   showFaultyDocuments: boolean;
 
+  categories: Category[];
+  
+
   getFilteredDocs(docs: BDDocument[]): Category[] {
 
-    let categories = new Array<Category>();
-    categories.push(new Category('Kontoudskrifter'));
-    categories.push(new Category('Deopt udtog'));
-    categories[0].addDocument(docs[0]);
-    categories[0].addDocument(docs[1]);
-    categories[1].addDocument(docs[1]);
+    this.categories = new Array<Category>();
 
-    return categories;
+    for (let doc of docs) {
+      if (this.checkDate(doc) && this.checkNetBank(doc) && this.checkFaulty(doc)) {
+        
+        if (this.dividedBy === 'Dokument type') {
+          this.addToCategories(doc.documentType, doc);
+        } else if (this.dividedBy === 'Dato') {
+          this.addToCategories(Filter.monthNames[doc.uploadedDate.getMonth()], doc);
+        } else if (this.dividedBy === 'Netbox') {
+          this.addToCategories(doc.documentType, doc);
+        }
+
+      }
+    }
+
+    return this.categories;
+  }
+
+  addToCategories(categoryName: string, doc: BDDocument) {
+    let exists: boolean;
+    exists = false;
+
+    for (let cat of this.categories) {
+      if (cat.name === categoryName) {
+        cat.addDocument(doc);
+        exists = true;
+      }
+    }
+    if (!exists) {
+      let newCat = new Category(categoryName);
+      newCat.addDocument(doc);
+
+      this.categories.push(newCat);
+    }
+
+  }
+
+  checkDate(doc: BDDocument): boolean {
+    if (doc === null) {return false;}
+
+    if (doc.uploadedDate >= new Date(this.dateFrom) && doc.uploadedDate <= new Date(this.dateTo)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  checkNetBank(doc: BDDocument): boolean {
+    if (doc === null) {return false;}
+    
+    if (this.showOnlyVisibleInNetBank && !doc.visibleInNetBox) {
+      return false;
+    }
+
+    return true;
+  }
+
+  checkFaulty(doc: BDDocument): boolean {
+    if (doc === null) {return false;}
+    
+    if (!this.showFaultyDocuments && doc.markedAsFaulty) {
+      return false;
+    }
+
+    return true;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,6 @@ import {Filter} from './Classes/filter';
 })
 export class AppComponent implements OnInit {
   results: BDDocument[];
-  root = 'http://localhost:8080/api.php?id=10';
   filter: Filter;
 
   private _filteredResults: Category[];
@@ -21,20 +20,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //    // Make the HTTP request: 
-    //    console.log('Starting http request');
-    //    this.http.get(this.root).subscribe(
-    //
-    //      data => {
-    //        // Read the result field from the JSON response.
-    //        this.results = data['data'];
-    //        console.log(data['data']);
-    //      },
-    //
-    //      err => {
-    //        console.log('Something went wrong');
-    //      }
-    //    );
   }
 
   onSearched(searchResult: BDDocument[]) {
