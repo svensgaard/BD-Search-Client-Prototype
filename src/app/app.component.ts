96b520c94610d12e2ced1e3bcb383cc40e7fab46
Mads <hypermads@gmail.com>
2017-11-17 10:51:04 +0100

    refnummer in URL :christmas_tree:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { BDDokType } from './Classes/BDDokType';
-import {BDDocument} from './Classes/bddocument';
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Category} from './Classes/category';
-import {Filter} from './Classes/filter';
+import { BDDocument } from './Classes/bddocument';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from './Classes/category';
+import { Filter } from './Classes/filter';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,29 +14,39 @@ import {Filter} from './Classes/filter';
 export class AppComponent implements OnInit {
   results: BDDocument[];
   private _filter: Filter;
-  refnummer= '2057632';
+  refnummer = '';
   private _filteredResults: Category[];
-  displayError= 'none';
+  displayError = 'none';
+  sub: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
     this._filter = new Filter();
     this._filteredResults = null;
   }
 
   ngOnInit(): void {
-    //First filter is everything and on dok type
-    let firstFilter = new Filter();
-    firstFilter.dividedBy = 'Dokument type';
-    firstFilter.sortBy = 'Faldende';
+    this.sub = this.route.params.subscribe(params => {
+      this.refnummer = params['refnummer'];
 
-    firstFilter.showOnlyVisibleInNetBank = false;
-    firstFilter.showFaultyDocuments = false;
-    firstFilter.includeAutoGen = true;
+      if(this.refnummer === null || this.refnummer === undefined) {this.refnummer = '2057642';}
 
-    this.filter = firstFilter;
+      //First filter is everything and on dok type
+      let firstFilter = new Filter();
+      firstFilter.dividedBy = 'Dokument type';
+      firstFilter.sortBy = 'Faldende';
+
+      firstFilter.showOnlyVisibleInNetBank = false;
+      firstFilter.showFaultyDocuments = false;
+      firstFilter.includeAutoGen = true;
+
+      this.filter = firstFilter;
+
+
+    });
+
   }
 
-  get filter():Filter {
+  get filter(): Filter {
     return this._filter;
   }
   set filter(newFilter: Filter) {
@@ -59,7 +70,7 @@ export class AppComponent implements OnInit {
   }
 
   onDokTypes(dokTypes: BDDokType[]) {
-    this._filter.dokTyper =  dokTypes;
+    this._filter.dokTyper = dokTypes;
   }
 
   get filteredResults(): Category[] {
@@ -67,11 +78,11 @@ export class AppComponent implements OnInit {
   }
 
   get numOfResults(): number {
-    if(this._filteredResults !== undefined && this._filteredResults !== null) {
+    if (this._filteredResults !== undefined && this._filteredResults !== null) {
       let num = 0;
-      for(let cat of this._filteredResults) {
+      for (let cat of this._filteredResults) {
         num += cat.documents.length;
-      }     
+      }
       return num;
     } else {
       return 0;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RootComponent } from './real.root.component';
 import { DoktyperService } from './doktyper.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,13 +9,13 @@ import {NgxPopperModule} from 'ngx-popper';
 import { AppComponent } from './app.component';
 
 import {HttpClientModule} from '@angular/common/http';
-
 import { DocumentsComponent } from './documents/documents.component';
 import { FilterComponent } from './filter/filter.component';
 import { SearchComponent } from './search/search.component';
 import { CategoryComponent } from './category/category.component';
 import { DocumentService } from './document.service';
 import { PopperContentComponent } from './popper-content/popper-content.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -24,16 +25,18 @@ import { PopperContentComponent } from './popper-content/popper-content.componen
     FilterComponent,
     SearchComponent,
     CategoryComponent,
-    PopperContentComponent
+    PopperContentComponent,
+    RootComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     Ng2AutoCompleteModule,
-    NgxPopperModule
+    NgxPopperModule,
+    AppRoutingModule
   ],
   providers: [DocumentService, DoktyperService],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
