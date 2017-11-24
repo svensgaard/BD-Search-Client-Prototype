bd2006c2803df2be89b560d834ff0f295dba3caa
David Dyrholm <ddyrholm@gmail.com>
2017-11-24 08:48:58 +0100

    Removed old filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { AppComponent } from './app.component';
 
 import {HttpClientModule} from '@angular/common/http';
 import { DocumentsComponent } from './documents/documents.component';
-import { FilterComponent } from './filter/filter.component';
 import { SearchComponent } from './search/search.component';
 import { CategoryComponent } from './category/category.component';
 import { DocumentService } from './document.service';
@@ -26,7 +25,6 @@ import {ToastModule} from 'ng2-toastr';
   declarations: [
     AppComponent,
     DocumentsComponent,
-    FilterComponent,
     SearchComponent,
     CategoryComponent,
     PopperContentComponent,
